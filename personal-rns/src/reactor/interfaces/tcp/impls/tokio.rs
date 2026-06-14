995af221929f152fc8d4a63de2ac476e02b63936
KenAKAFrosty <90424167+KenAKAFrosty@users.noreply.github.com>
2026-06-13 22:17:51 -0500

    Import HostCommand into the tcp tokio test module so the lib's tcp unit tests compile under --features tcp

--- personal-rns/src/reactor/interfaces/tcp/impls/tokio.rs
+++ personal-rns/src/reactor/interfaces/tcp/impls/tokio.rs
@@ -207,7 +207,7 @@ mod tests {
     use super::*;
     use crate::interfaces::rns_serial_framing::{self, RnsSerialDecoder, ESC, FLAG};
     use crate::reactor::grant::{GrantConsumer, GrantProducer};
-    use crate::reactor::impls::tokio_reactor::{tokio_grant_lane, TokioGrantConsumer};
+    use crate::reactor::impls::tokio_reactor::{tokio_grant_lane, HostCommand, TokioGrantConsumer};
     use tokio::io::{AsyncReadExt, AsyncWriteExt};
     use tokio::sync::mpsc::{self, UnboundedSender};
 
