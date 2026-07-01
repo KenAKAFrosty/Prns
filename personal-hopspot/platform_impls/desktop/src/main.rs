af6b8a965cfc469e9ec669aa47be842c2af1abb9
KenAKAFrosty <90424167+KenAKAFrosty@users.noreply.github.com>
2026-07-01 12:22:57 -0500

    hopspot: split app into desktop and esp32 platform impls

---
+++ personal-hopspot/platform_impls/desktop/src/main.rs
@@ -0,0 +1,7 @@
+mod desktop;
+mod host_serial;
+mod host_usb;
+
+fn main() {
+    desktop::run();
+}
--- personal-hopspot/platform_impls/esp32/src/main.rs
+++ personal-hopspot/platform_impls/esp32/src/main.rs
@@ -1,8 +1,7 @@
-#![cfg_attr(target_os = "none", no_std)]
-#![cfg_attr(target_os = "none", no_main)]
+#![no_std]
+#![no_main]
 #![cfg_attr(target_arch = "xtensa", feature(asm_experimental_arch))]
 
-#[cfg(target_os = "none")]
 extern crate alloc;
 
 #[cfg(any(
@@ -10,9 +9,6 @@ extern crate alloc;
     all(target_arch = "riscv32", feature = "ble-bringup-c6")
 ))]
 mod ble;
-#[cfg(not(target_os = "none"))]
-mod desktop;
-#[cfg(target_os = "none")]
 mod engine_storage;
 #[cfg(target_arch = "riscv32")]
 mod esp32c6;
@@ -20,17 +16,8 @@ mod esp32c6;
 mod esp32s3;
 #[cfg(all(target_arch = "xtensa", not(feature = "board-tbeam-supreme")))]
 mod heltec_v4;
-#[cfg(not(target_os = "none"))]
-mod host_serial;
-#[cfg(not(target_os = "none"))]
-mod host_usb;
 #[cfg(all(target_arch = "xtensa", feature = "board-tbeam-supreme"))]
 mod t_beam_supreme;
-
-#[cfg(not(target_os = "none"))]
-fn main() {
-    desktop::run();
-}
 
 #[cfg(target_arch = "riscv32")]
 #[esp_rtos::main]
