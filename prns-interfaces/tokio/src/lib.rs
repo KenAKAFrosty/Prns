d89f1a012a520f4945c86a0d262c3964af9d2110
KenAKAFrosty <90424167+KenAKAFrosty@users.noreply.github.com>
2026-07-02 12:00:08 -0500

    prns-interfaces-tokio: move serial/kiss/rnode/pipe

--- prns-interfaces/tokio/src/lib.rs
+++ prns-interfaces/tokio/src/lib.rs
@@ -16,3 +16,15 @@ pub mod tcp;
 
 #[cfg(feature = "udp")]
 pub mod udp;
+
+#[cfg(feature = "serial")]
+pub mod serial;
+
+#[cfg(feature = "kiss")]
+pub mod kiss;
+
+#[cfg(feature = "rnode")]
+pub mod rnode;
+
+#[cfg(feature = "pipe")]
+pub mod pipe;
