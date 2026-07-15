dc358c429951c3e41123bc795f3e998709a152b0
KenAKAFrosty <90424167+KenAKAFrosty@users.noreply.github.com>
2026-07-15 16:03:36 -0500

    Type the RNS 1.3.8 RPC wire boundary

---
+++ fuzz/fuzz_targets/shared_instance_rpc_request_msgpack.rs
@@ -0,0 +1,11 @@
+#![no_main]
+
+#[allow(dead_code)]
+#[path = "../../prns-interfaces/tokio/src/shared_instance/rpc_compat/request.rs"]
+mod request;
+
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &[u8]| {
+    let _ = request::decode(data);
+});
