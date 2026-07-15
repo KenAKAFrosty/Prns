6b898d8748b70e1f47d24f23f78719eaa89ec9e0
KenAKAFrosty <90424167+KenAKAFrosty@users.noreply.github.com>
2026-07-15 14:19:31 -0500

    Consolidate routing feature gates

--- prns-core/src/routing/links/resources/table/impls/mod.rs
+++ prns-core/src/routing/links/resources/table/impls/mod.rs
@@ -1,12 +1,18 @@
 mod fixed;
 pub use fixed::FixedResourceTable;
 
-#[cfg(feature = "alloc")]
-mod heap;
-#[cfg(feature = "alloc")]
-pub use heap::{HeapResourceTable, DEFAULT_MAX_RESOURCES};
+cfg_if::cfg_if! {
+    if #[cfg(feature = "alloc")] {
+        mod heap;
 
-#[cfg(feature = "external-alloc")]
-mod fixed_heap;
-#[cfg(feature = "external-alloc")]
-pub use fixed_heap::FixedHeapResourceTable;
+        pub use heap::{HeapResourceTable, DEFAULT_MAX_RESOURCES};
+    }
+}
+
+cfg_if::cfg_if! {
+    if #[cfg(feature = "external-alloc")] {
+        mod fixed_heap;
+
+        pub use fixed_heap::FixedHeapResourceTable;
+    }
+}
