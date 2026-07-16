806a109206b6e309aae6944c9e12c1a112843211
KenAKAFrosty <90424167+KenAKAFrosty@users.noreply.github.com>
2026-07-15 22:54:43 -0500

    Implement Tokio routing control

--- prns-core/src/engine/management.rs
+++ prns-core/src/engine/management.rs
@@ -1,12 +1,14 @@
 use crate::routing::RemovedRoute;
-use crate::storage::StorageLayout;
+use crate::storage::{DirtyInterfaceSet, StorageLayout};
 use crate::wire::{DestinationHash, TransportId};
 
 use super::EngineState;
 
 impl<S: StorageLayout> EngineState<S> {
     pub fn drop_route(&mut self, destination: &DestinationHash) -> Option<RemovedRoute> {
-        self.routing_table.drop_route(destination)
+        let removed = self.routing_table.drop_route(destination)?;
+        self.dirty_interfaces.mark(removed.receiving_interface);
+        Some(removed)
     }
 
     pub fn drop_routes_via(
@@ -14,6 +16,11 @@ impl<S: StorageLayout> EngineState<S> {
         transport: TransportId,
         on_removed: &mut impl FnMut(RemovedRoute),
     ) -> usize {
-        self.routing_table.drop_routes_via(transport, on_removed)
+        let dirty = &mut self.dirty_interfaces;
+        self.routing_table
+            .drop_routes_via(transport, &mut |removed| {
+                dirty.mark(removed.receiving_interface);
+                on_removed(removed);
+            })
     }
 }
