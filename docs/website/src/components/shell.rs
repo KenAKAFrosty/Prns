5e11f4ed4a4b95357647800ac32cd39af0fa686a
Prns Tests <tests@example.test>
2026-08-01 22:33:08 -0500

    Return to the top on route navigation

--- docs/website/src/components/shell.rs
+++ docs/website/src/components/shell.rs
@@ -1,9 +1,23 @@
+use dioxus::document;
 use dioxus::prelude::*;
 
 use super::{Footer, TopNav};
 
 #[component]
 pub fn Shell() -> Element {
+    let route = use_route::<crate::routes::Route>();
+    let mut last_route = use_signal(|| None::<crate::routes::Route>);
+    use_effect(use_reactive!(|route| {
+        let arrived_by_navigation = last_route
+            .peek()
+            .as_ref()
+            .is_some_and(|previous| *previous != route);
+        if arrived_by_navigation {
+            document::eval("window.scrollTo(0, 0);");
+        }
+        last_route.set(Some(route));
+    }));
+
     rsx! {
         div { class: "min-h-screen flex flex-col bg-ink text-paper",
             TopNav {}
