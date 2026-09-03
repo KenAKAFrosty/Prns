87fb12ce04a88eebbe7dcf260294da7c4d01d6e9
evelant <andrew@taskheroics.com>
2026-09-03 09:50:32 -0400

    Handle E290 pairing expiry during authorization

--- personal-hopspot/embedded/esp32/src/s3/mod.rs
+++ personal-hopspot/embedded/esp32/src/s3/mod.rs
@@ -435,6 +435,11 @@ fn firmware_on_event(event: PrnsEvent<'_>, _state: &()) {
         }) => {
             remote_control_authorization_persisted(attempt_id);
         }
+        PrnsEvent::Message(Message::RemoteControlTargetPairingExpiredDuringAuthorization {
+            attempt_id,
+        }) => {
+            let _ = update_remote_control_state(|state| state.expired(Some(attempt_id)));
+        }
         PrnsEvent::Message(Message::RemoteControlTargetPairingExpired { aborted }) => {
             let attempt_id = aborted.attempt_id();
             let _ = update_remote_control_state(|state| state.expired(Some(attempt_id)));
